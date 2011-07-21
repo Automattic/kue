b34fa64226dd39b06467b51ada195dca3bc006af
Tj Holowaychuk <tj@vision-media.ca>
2011-07-21 10:57:18 -0700

    example

---
+++ examples/delayed.js
@@ -0,0 +1,21 @@
+
+var kue = require('../');
+
+// create our job queue
+
+var jobs = kue.createQueue();
+
+// one minute
+
+var minute = 60000;
+
+jobs.create('email', {
+    title: 'Account renewal required'
+  , to: 'tj@learnboost.com'
+  , template: 'renewal-email'
+}).delay(minute)
+  .priority('high')
+  .save();
+
+// start the UI
+kue.app.listen(3000);
