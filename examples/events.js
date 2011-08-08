5ef0b812227df45f0be4cf3f02ebda309e3a5daa
Tj Holowaychuk <tj@vision-media.ca>
2011-08-08 10:05:28 -0700

    Fixed `Job#remove(fn)`
    removes:
    
      - hash
      - search index
      - from state set
      - from jobs set

--- examples/events.js
+++ examples/events.js
@@ -21,9 +21,9 @@ function create() {
   });
 
   job.on('complete', function(){
-      console.log("Job complete");
+      console.log(" Job complete");
   }).on('failed', function(){
-      console.log("Job failed");
+      console.log(" Job failed");
   }).on('progress', function(progress){
     process.stdout.write('\r  job #' + job.id + ' ' + progress + '% complete');
   });
