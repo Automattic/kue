984687856013437a3bf6975d5da374d836bd5fe9
Tj Holowaychuk <tj@vision-media.ca>
2011-08-16 09:47:34 -0700

    Added many.js example

---
+++ examples/many.js
@@ -0,0 +1,51 @@
+
+var kue = require('../')
+  , express = require('express');
+
+// create our job queue
+
+var jobs = kue.createQueue();
+
+function create() {
+  var name = ['tobi', 'loki', 'jane', 'manny'][Math.random() * 4 | 0];
+  jobs.create('video conversion', {
+      title: 'converting ' + name + '\'s to avi'
+    , user: 1
+    , frames: 200
+  }).save();
+  setTimeout(create, Math.random() * 3000 | 0);
+}
+
+create();
+
+function create2() {
+  var name = ['tobi', 'loki', 'jane', 'manny'][Math.random() * 4 | 0];
+  jobs.create('email', {
+      title: 'emailing ' + name + ''
+    , body: 'hello'
+  }).save();
+  setTimeout(create2, Math.random() * 1000 | 0);
+}
+
+create2();
+
+// process video conversion jobs, 3 at a time.
+
+jobs.process('video conversion', 2, function(job, done){
+  console.log('video');
+  setTimeout(done, Math.random() * 5000);
+});
+
+// process 10 emails at a time
+
+jobs.process('email', 10, function(job, done){
+  console.log('email');
+  setTimeout(done, Math.random() * 2000);
+});
+
+// start the UI
+var app = express.createServer();
+app.use(express.basicAuth('foo', 'bar'));
+app.use(kue.app);
+app.listen(3000);
+console.log('UI started on port 3000');
