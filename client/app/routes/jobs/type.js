34820f3ef68603c3929e5dfe96cca165339569d4
Arnaud Benard <arnaud@streethub.com>
2014-11-25 17:02:23 +0000

    added table template and other stuff;

--- client/app/routes/jobs/type.js
+++ client/app/routes/jobs/type.js
@@ -4,10 +4,13 @@ import Job from '../../models/job';
 export default Ember.Route.extend({
 
     model: function(params) {
+        console.log('//////////////same route', params);
+        this.controllerFor('jobs.type').set('type', params.type);
         return Job.find({
             type: params.type,
             state: 'active',
             page: 1
         });
-    }
+    },
+
 });
